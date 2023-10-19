2eae7a5fce3d3d7e8861e164041796ae5434f926
unknown <daniel.hurtadoramirez314@gmail.com>
2023-10-19 16:50:49 +0200

    more build fiddling

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ command, mode, ssrBuild }) => {
-  if (command === 'serve') {
+  if (command === 'serve' || mode === "include_frame") {
     // During development, we want to preview our game as hosted on itch.io
     return {
+      base: './',
       build: {
+        outDir: './dist',
+        emptyOutDir: true,
         rollupOptions: {
-          input: './itchio.html'
+          input: ['./index.html', './src/index.html']
         },
         target: 'esnext'
       },
-      server: {
-        open: 'itchio.html'
-      }
     }
   } else if (command === 'build') {
     // At build time, we only build the game itself, not the itch.io container
